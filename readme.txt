5986378f39d4300c518679fdf3d9763c20288b7f
andrefarzat <andrefarzat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-08 13:20:51 +0000

    Creating version 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/wp-plotly/tags/1.0.1@1130407 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,17 @@
+=== Plugin Name ===
+Contributors: André Farzat
+Donate link: http://plot.ly/
+Tags: plotly, plot.ly, embed, graph, graphs
+Requires at least: 3.0.1
+Tested up to: 4.1
+Stable tag: 4.1
+License: MIT
+License URI: http://opensource.org/licenses/MIT
+
+Embed Plotly graphs in wordpress admin.
+
+== Description ==
+
+This plugin is for quick inclusion of interactive Plotly graphs within your Wordpress blog.
+It works the same way as YouTube embeds — All you have to do is paste the link to your Plotly graph into Wordpress and the interactive graph will appear.
+For example, paste http://plot.ly/~chris/1638 into the Wordpress editor, and the graph with this URL will be displayed.
---
+++ wp-plotly.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Plugin Name: Plotly
+ * Plugin URI: http:s//github.com/plotly/wp-plotly
+ * Description: Embed plotly graphs in your posts
+ * Version: 1.0.0
+ * Author: André Farzat
+ * Author URI: https://plot.ly
+ * License: GPL2
+ */
+
+
+
+add_action( 'init', 'plotly_add_embed_handlers' );
+function plotly_add_embed_handlers() {
+    wp_embed_register_handler( 'plotly', '#https?://(www\.)?plot\.ly/.*#i', 'plotly_embed_handler' );
+}
+
+function plotly_embed_handler($matches, $attr, $url, $rawattr){
+
+    $parsed = parse_url($url);
+    $items = explode('/', $parsed['path']);
+    $username = substr($items[1], 1);
+    $idlocal = explode('.', $items[2]);
+    $idlocal = $idlocal[0];
+
+    $localurl = "https://plot.ly/~$username/$idlocal";
+    $image = "<img src='${localurl}.png' onerror='this.onerror=null;this.src=\"https://plot.ly/404.png\"' />";
+
+
+    if( empty($username) || empty($idlocal) ){
+        return apply_filters( 'embed_plotly', $url, $matches, $attr, $url, $rawattr );
+    }
+
+    if( is_admin() ){
+        $embed = $image;
+    } else {
+        $embed = "<div>";
+        $embed .= "<a href='{$localurl}' target='_blank'>$image</a>";
+        $embed .= "<script data-plotly='${username}:{$idlocal}' src='https://plot.ly/embed.js' async></script>";
+        $embed .= "</div>";
+    }
+
+    return apply_filters( 'embed_plotly', $embed, $matches, $attr, $url, $rawattr );
+}
+
+
+?>
